48833a08fd2b35d8182870a6b22b14f9cb90f8a2
gator-boi <gator-boi@users.noreply.github.com>
2023-01-20 18:17:13 +0000

    make state contract abstract

--- evm/src/token-bridge-relayer/TokenBridgeRelayerState.sol
+++ evm/src/token-bridge-relayer/TokenBridgeRelayerState.sol
@@ -55,7 +55,7 @@ abstract contract TokenBridgeRelayerStorage {
     }
 }
 
-contract TokenBridgeRelayerState {
+abstract contract TokenBridgeRelayerState {
     TokenBridgeRelayerStorage.State _state;
 }
 
