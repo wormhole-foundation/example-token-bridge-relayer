9e50c677ebed1ce7e97eeca8c2e58ab109676f56
Sebastián Claudio Nale <sebinale@gmail.com>
2023-05-31 17:06:29 -0300

    evm: adds a length factor in batch updates to gas limit in polygon.

--- evm/ts/scripts/registerTokens.ts
+++ evm/ts/scripts/registerTokens.ts
@@ -104,7 +104,7 @@ async function updateSwapRate(
   if (RELEASE_CHAIN_ID === 5) {
     // Polygon
     overrides.type = 0;
-    overrides.gasLimit = 60_000;
+    overrides.gasLimit = 60_000 + 21_000 * batch.length;
   } else if (RELEASE_CHAIN_ID === 10) {
     // Fantom
     overrides.type = 0;
