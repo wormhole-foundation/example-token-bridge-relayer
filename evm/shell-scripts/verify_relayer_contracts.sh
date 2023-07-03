39614ae7f8604bdc6c90721c61747c25c29d8257
Sebastián Claudio Nale <sebinale@gmail.com>
2023-07-03 12:49:10 -0300

    evm: Simplifies the contract verification script.

--- evm/shell-scripts/verify_relayer_contracts.sh
+++ evm/shell-scripts/verify_relayer_contracts.sh
@@ -1,18 +1,17 @@
 #/bin/bash
 
-chain_id=$1
-optimizer_runs=$2
-etherscan_key=$3
+etherscan_key=$1
 
 # environment variables
-wormhole_chain=$RELEASE_WORMHOLE_CHAIN_ID
-wormhole_addr=$RELEASE_WORMHOLE_ADDRESS
+evm_chain_id=$RELEASE_EVM_CHAIN_ID
 bridge_addr=$RELEASE_BRIDGE_ADDRESS
 weth_addr=$RELEASE_WETH_ADDRESS
+fee_recipient=$RELEASE_FEE_RECIPIENT
+owner_assistant=$RELEASE_OWNER_ASSISTANT
 unwrap=$RELEASE_UNWRAP_WETH
 deployed_addr=$RELAYER_CONTRACT_ADDRESS
 
-forge verify-contract --chain-id $chain_id --num-of-optimizations $optimizer_runs --watch \
---constructor-args $(cast abi-encode "constructor(uint16,address,address,address,bool)" $wormhole_chain $wormhole_addr $bridge_addr $weth_addr $unwrap) \
-    --compiler-version v0.8.17 $deployed_addr \
-    src/token-bridge-relayer/TokenBridgeRelayer.sol:TokenBridgeRelayer $etherscan_key
+forge verify-contract --chain-id $evm_chain_id --watch --etherscan-api-key $etherscan_key \
+--constructor-args $(cast abi-encode "constructor(address,address,address,address,bool)" $bridge_addr $weth_addr $fee_recipient $owner_assistant $unwrap) \
+    $deployed_addr \
+    src/token-bridge-relayer/TokenBridgeRelayer.sol:TokenBridgeRelayer
