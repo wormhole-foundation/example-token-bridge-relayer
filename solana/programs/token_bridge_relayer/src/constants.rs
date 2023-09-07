9be8f6d6468f378efd40641a9b99025300d2bb12
gator-boi <gator-boi@users.noreply.github.com>
2023-09-07 11:41:19 -0400

    solana: update Makefile and remove stale comments

--- solana/programs/token_bridge_relayer/src/constants.rs
+++ solana/programs/token_bridge_relayer/src/constants.rs
@@ -1,11 +1,9 @@
 use anchor_lang::prelude::constant;
 
 #[constant]
-// AKA `b"bridged"`.
 pub const SEED_PREFIX_BRIDGED: &[u8] = b"bridged";
 
 #[constant]
-/// AKA `b"tmp"`.
 pub const SEED_PREFIX_TMP: &[u8] = b"tmp";
 
 #[constant]
